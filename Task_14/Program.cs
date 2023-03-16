051c7d282c9ecc31fe2232de1a4451656f0b5e68
felixpack <felix.ru91@gmail.com>
2023-03-16 12:18:21 +0300

    Семинар 2 закончен

---
+++ Task_14/Program.cs
@@ -0,0 +1,11 @@
+﻿// напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 161 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+// || - или
+// && - и
+
---
+++ Task_16/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа которая на вход принимает 2 числа и проверяет, является ли одно число, квадратом другого
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m || m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
