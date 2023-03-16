35d59779b8a56ee4f3c96508c556b19803d3d2db
felixpack <felix.ru91@gmail.com>
2023-03-16 21:35:45 +0300

    Домашка ко 2 семинару

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.Write("Вы ошиблись!\nВведите Трёхзначное число (от 100 до 999): ");
+    n = Convert.ToInt32(Console.ReadLine());
+} 
+Console.WriteLine($"Введенное трёхзначное число {n}");
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine($"Вторая цифра: {(n - n1 * 100 - n3) /10}");
--- Task_16/obj/Debug/net6.0/Task_16.AssemblyInfo.cs
+++ Task_16/obj/Debug/net6.0/Task_16.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task_1_domashka")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task_1_domashka")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task_1_domashka")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_16")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
