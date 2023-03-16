44c5c75f00186dcea1e337940427159da98efeb0
felixpack <felix.ru91@gmail.com>
2023-03-16 21:36:16 +0300

    Домашка ко 2 семинару

---
+++ Task_15/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день
+//недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите день недели(от 1 до 7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n == 6 & n == 7)
+{
+    Console.Write("Это выходной день!");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели(от 1 до 7): ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if 
+ (n == 1)
+    Console.WriteLine("Понедельник");
+else if (n == 2)
+    Console.WriteLine("Вторник");
+else if (n == 3)
+    Console.WriteLine("Среда");
+else if (n == 4)
+    Console.WriteLine("Четверг");
+else if (n == 5)
+    Console.WriteLine("Пятница");
+else if (n == 6)
+    Console.WriteLine("Суббота");
+else
+    Console.WriteLine("Воскресенье");
+if(n == 6)
+  Console.WriteLine("это выходной день");
+else if(n == 7)
+  Console.WriteLine("это выходной день");
