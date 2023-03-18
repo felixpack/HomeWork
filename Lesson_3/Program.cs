4927418b652a335d55d29df383db675c4fdd22b2
felixpack <felix.ru91@gmail.com>
2023-03-18 22:37:46 +0300

    Lesson 3

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая будет находить сумму цифр числа (цифры 0 -9)
+Console.Clear();
+string s = Console.ReadLine()!;
+int sum = 0;
+//теория на уроке
+
+// // ! исключаем значение null
+// Char n = '6';
+// Console.WriteLine(Convert.ToInt32(s[0].ToString()) * 10);
+
+for (int i = 0; i < s.Length; i++)
+    sum = sum + int.Parse(s[i].ToString());
+Console.WriteLine(sum);
---
+++ Task_17/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (х,у),
+// причем х и у не равны 0 и выдает номер четверти плоскости, в которой находится точка
+
+Console.Clear();
+Console.Write("Введите координату Х: ");
+// ввод дробных чисел. т.к. пользователь может ввести и такое
+double x = Convert.ToDouble(Console.ReadLine()); 
+//если хотим ввести дробную часть при вводе, то через точку
+// double n = 1.56
+Console.Write("Введите координату y: ");
+double y  = Convert.ToDouble(Console.ReadLine());
+while (x==0) //Если пользователь ошибется в Х, а У верна
+{
+    Console.Write("Вы ошиблись!\nВведите координату х: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y==0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+if(x > 0 && y > 0)
+    Console.WriteLine("1");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3");
+else
+    Console.WriteLine("4");
+
+// ответ: в терминале выводим через запятую дробные числа
+
+
+
+
