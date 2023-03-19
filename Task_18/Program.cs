0c9b8cdcb689c3b898cb33dde218e12c32fa6ce4
felixpack <felix.ru91@gmail.com>
2023-03-19 20:16:18 +0300

    Домашка 3 урока

---
+++ Task_18/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу, которая по заданному номеру четверти, 
+//покажет дипозон возможных координат точек в этой четверти
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 4)
+{
+    Console.Write("Вы ошиблись!\nВведите номер четверти: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else
+    Console.WriteLine("x > 0 and y < 0");
+
+// значения(-8,1) и (4,+8) не подходят нам
+//(1,4) подходят
---
+++ Task_22/Program.cs
@@ -0,0 +1,8 @@
+﻿// ...принимает на вход число (N) и выдает таблицу квадратов чисел 
+//от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} ^ 2 = {i * i}");
