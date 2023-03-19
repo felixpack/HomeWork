d27b80b35aa622acd53dc32eeb5cd7bb74ca8ada
felixpack <felix.ru91@gmail.com>
2023-03-19 20:17:00 +0300

    Домашка 3 урока

---
+++ Task_20/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// ..., которая принимает на вход координаты 2 точек и находит 
+//hасстояние между ними в 2Д пространстве
+
+//double не целое число
+
+Console.Clear();
+Console.Write("Введите координату Х(1): ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y(1): ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x(2): ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y(2): ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {Math.Round(s, 2)}");
