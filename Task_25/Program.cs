7b7a84fc99ca7efe580bf40bdaf9d663b47b4d38
felixpack <felix.ru91@gmail.com>
2023-03-22 17:12:30 +0300

    Семминар 5

---
+++ Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать функцию Math.Pow()!
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{n} ^ m ");
+
+
+// for (int i = 1; i <= n; i++)
+//     Console.WriteLine($"{n} ^ m = {m * m * m}");
---
+++ Task_28/Program.cs
@@ -0,0 +1,11 @@
+﻿// напишите программу, которая принимает  на вход число N и
+// выдает произведение чисел от 1 до N (1*2*3*4 =24)
+
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 1;
+for (int i = 2; i <= n; i++) // факториал 1 равен 1, по-этому с 2х
+    count = count * i;
+Console.WriteLine(count);
