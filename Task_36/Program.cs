e6508dba15a9b7b47add8ce3322183ea5aee5f3d
felixpack <felix.ru91@gmail.com>
2023-03-28 08:44:10 +0300

    Домашка к 5 уроку

---
+++ Task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10 , 10);
+}
+int SumNechet(int[] array)
+{   
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {   if(i % 2 != 0){
+        sum += array[i];}
+    }
+    return sum;
+}
+
+Console.Clear();
+int[] array = new int[5];
+InputArray(array);
+System.Console.WriteLine($"array: [{string.Join(" , " , array)}]");
+System.Console.WriteLine($"Сумма элементов на нечетных позициях: {SumNechet(array)}");
---
+++ Task_38/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Задайте массив вещественных чисел. Найдите разницу между максимальным
+//  и минимальным элементов массива.
+
+// [3.03 7.85 22.13 2.05 78.48] -> 76.43
+
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * 100 + 0, 2);
+}
+double Max(double[]array)
+{
+    double max = array[0];
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (max<array[i])
+        {
+            max=array[i];
+        }
+    }
+    return max;
+}
+double Min(double[]array)
+{
+    double min = array[0];
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (min>array[i])
+        {
+            min=array[i];
+        }
+    }
+    return min;
+}
+
+Console.Clear();
+double[] array = new double[5];
+InputArray(array);
+System.Console.WriteLine($"array: [{string.Join(" , " , array)}]");
+System.Console.WriteLine($"Max {Max(array)} - Min {Min(array)} = {Max(array)-Min(array)}");
