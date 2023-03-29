e410c19bf934b3772e0974e3abc19e27288161a0
felixpack <felix.ru91@gmail.com>
2023-03-29 16:52:55 +0300

    Домашка допы к 4 семинару

---
+++ Dop_zadacha_Gipoteza_Goldbaha/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int countDel = 0, i, j, k, m;
+for (i = 2; i <= Math.Sqrt(n) + 1; i++)
+{
+    countDel = 0;
+    for (j = 2; j <= Math.Sqrt(i) + 1; j++)
+    {
+        if (i % j == 0)
+            countDel++;
+    }
+    if (countDel == 0)
+    {
+        m = n - i;
+        for (k = 2; k <= Math.Sqrt(m) + 1; k++)
+        {
+            if (m % k == 0)
+                countDel++;
+        }
+        if (countDel == 0)
+        {
+            Console.WriteLine($"{i} {m}");
+            return;
+        }
+    }
+}
---
+++ Dop_zadacha_super_sdvig/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] beginArray = new int[n];
+int[] resultArray = new int[n];
+for (int i = 0; i < n; i++)
+    beginArray[i] = new Random().Next(1, 11); // [1, 10]
+Console.WriteLine($"Начальный массив: [{string.Join(", ", beginArray)}]");
+Console.Write("Введите на сколько элементов Вы хотите сдвинуть массив: ");
+int k = int.Parse(Console.ReadLine()!);
+k = k % n;
+if (k > 0)
+{
+    for (int i = 0; i < k; i++)
+        resultArray[i] = beginArray[n - k + i];
+    for (int i = 0; i < n - k; i++)
+        resultArray[k + i] = beginArray[i];
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", resultArray)}]");
+}
+else
+{
+    k = -k;
+    for (int i = 0; i < k; i++)
+        resultArray[n - k + i] = beginArray[i];
+    for (int i = 0; i < n - k; i++)
+        resultArray[i] = beginArray[k + i];
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", resultArray)}]");
+}
