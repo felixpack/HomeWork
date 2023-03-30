ca8f0f828f2f0f15df0789917ed77749b573764f
felixpack <felix.ru91@gmail.com>
2023-03-30 13:08:30 +0300

    6  семинар

---
+++ Task_44/Program.cs
@@ -0,0 +1,14 @@
+﻿//не используя рекурсию, выведите первые N чисел Фибоначчи. первые 2 чсла Фиббоначчи:0 и 1
+//Если n = 5 => 01123
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int a0 = 0, a1 = 1, x;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{a0} ");
+    x = a0 + a1;
+    a0 = a1;
+    a1 = x;
+}
---
+++ Task_45/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//которая будет создавать копию заданного массива с помощью поэлементного капирования
+
+
+Console.Clear();
+int[] a = {1, 2, 3, 4, 5};
+int[] b = new int[a.Length];
+
+for (int i = 0; i < a.Length; i++)
+    b[i] = a[i];
+
+b[0] = b[0] + 10;
+Console.WriteLine(string.Join(" ", a));
+Console.WriteLine(string.Join(" ", b));
