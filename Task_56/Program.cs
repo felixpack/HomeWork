48f419deff63135cca4f104eaf073a80df177c3e
felixpack <felix.ru91@gmail.com>
2023-04-06 20:18:31 +0300

    Домашка к 8 семинару. решение оставшихся задач

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -17,57 +17,53 @@ void InputMatrix(int[,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(0, 10);
-        }
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
     }
 }
+
+
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            System.Console.Write($"{matrix[i, j]} \t");
-
-        }
-        System.Console.WriteLine();
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
     }
 }
-void Sravnenie(int[,] matrix)
-{
 
-    int[] sum = new int[matrix.GetLength(0)];
-    for (int i = 0; i < matrix.GetLength(0); i++)
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int minRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+        minRow += matrix[0, i];
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
     {
+        int sumRow = 0;
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            sum[i] += matrix[i, j];
-
+            sumRow += matrix[i, j];
         }
+        if (minRow > sumRow)
+            minRow = sumRow;
     }
-    int min = 0;
-    int minLine = 0;
-    for (int i = 0; i < sum.Length; i++)
-    {
-        min = sum[0];
-        if (sum[i] <= min)
-        {
-            min = sum[i];
-            minLine = i;
-        }
-
-    }
-    System.Console.WriteLine($"Минимальная сумма в строке {minLine + 1} : {min}");
-
+    return minRow;
 }
-
 
 
 Console.Clear();
-System.Console.Write("Введите размерность массива: ");
-int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+Console.Write("Введите размер прямоугольного двумерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("Вы ошиблись!\nВведите размер прямоугольного двумерного массива: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
+Console.WriteLine("Начальный двумерный массив: ");
 PrintMatrix(matrix);
-Sravnenie(matrix);
+Console.WriteLine();
+Console.WriteLine($"Результат: {ReleaseMatrix(matrix)}");
