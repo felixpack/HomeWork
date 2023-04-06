38992ce77b900a47327b8f608236abae9fa8d064
felixpack <felix.ru91@gmail.com>
2023-04-06 21:41:22 +0300

    Урок 9 рекурсия

---
+++ Task_65/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+string f(int a, int b)
+{
+  if (a == b)
+    return $"{a} ";
+  return f(a, b - 1) + $"{b} "; //вызов функции
+}
+
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
---
+++ Task_69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+int f(int a, int b)
+{
+  if (b ==0)
+    return 1; 
+  return f(a, b - 1) * a;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n, m));
