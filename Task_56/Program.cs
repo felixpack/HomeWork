dc61189c8fc14e2c03c129bac47830ff6063bfac
felixpack <felix.ru91@gmail.com>
2023-04-06 20:00:04 +0300

    Домашка к 8 семинару

---
+++ Task_56/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+void Sravnenie(int[,] matrix)
+{
+
+    int[] sum = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum[i] += matrix[i, j];
+
+        }
+    }
+    int min = 0;
+    int minLine = 0;
+    for (int i = 0; i < sum.Length; i++)
+    {
+        min = sum[0];
+        if (sum[i] <= min)
+        {
+            min = sum[i];
+            minLine = i;
+        }
+
+    }
+    System.Console.WriteLine($"Минимальная сумма в строке {minLine + 1} : {min}");
+
+}
+
+
+
+Console.Clear();
+System.Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Sravnenie(matrix);
